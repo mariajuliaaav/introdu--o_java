10f8b7ce7ebd35e0cf863ad7405a99aaa8427c2e
Danielly Ramos <danielly.goncalves@estudante.iftm.edu.br>
2022-03-29 16:44:16 -0300

    first commit

---
+++ introdução.java
@@ -0,0 +1,5 @@
+public class introdução {
+    public static void main(String[] args){
+            System.out.print("- Laboratório de Programação 2 - LP2\n- 2 ano de Informática\n- Seja bem vindo!\n- Vamos aprender Java.");
+    }
+}
